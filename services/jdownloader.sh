76b6867ceeb3bc15650d41323f0b791b5a03517a
ppksgopin <ppksgopin@gmail.com>
2024-09-27 14:43:12 +0800

    move service away

--- services/jdownloader.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-mkdir -p /home/ppksgopin/Downloads/jdownloader/config
-sudo docker run -d --init --restart=always -v /home/ppksgopin/Downloads/jdownloader:/opt/JDownloader/Downloads -v /home/ppksgopin/Downloads/jdownloader/config:/opt/JDownloader/app/cfg --name jdownloader -p 3129:3129 -e MYJD_USER=ppksgopin@gmail.com -e MYJD_PASSWORD=Azxpolmppk0215 -e MYJD_DEVICE_NAME=ppksgopin jaymoulin/jdownloader 
