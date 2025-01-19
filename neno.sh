3392a640774e3c45af4e139d38acbeb13739442f
蔡孟儒 <ppksgopin@gmail.com>
2025-01-19 20:48:25 +0800

    Update neno.sh

--- neno.sh
+++ neno.sh
@@ -5,12 +5,9 @@ mkdir /etc/apt/apt.conf.d/off && mv /etc/apt/apt.conf.d/20apt-esm-hook.conf /etc
 apt-get update -y && apt --fix-broken install -y && apt-get upgrade -y
 apt-get install ibus-chewing -y
 
-
-apt purge kate -y
-apt autoremove -y
+apt purge kate -y && apt autoremove -y
 
 apt install qemu-system libguestfs-tools libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf swtpm -y
-
 
 usermod -a -G libvirt ppksgopin
 usermod -a -G kvm ppksgopin
@@ -23,6 +20,8 @@ sudo virsh net-autostart default
 apt install tree p7zip-full -y
 echo "ppksgopin  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ppksgopin
 
+apt install ssh firewalld -y
+systemctl start ssh && systemctl enable --now ssh
 
 apt-get update -y && apt --fix-broken install -y && apt-get upgrade -y
 
