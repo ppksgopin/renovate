f3a74cb45b57ca64aa7ff2840857aaf9d5ca55a4
蔡孟儒 <ppksgopin@gmail.com>
2025-01-19 20:37:40 +0800

    Add files via upload

---
+++ neno.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir /etc/apt/apt.conf.d/off && mv /etc/apt/apt.conf.d/20apt-esm-hook.conf /etc/apt/apt.conf.d/off  
+
+apt-get update -y && apt --fix-broken install -y && apt-get upgrade -y
+apt-get install ibus-chewing -y
+
+
+apt purge kate -y
+apt autoremove -y
+
+apt install qemu-system libguestfs-tools libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf swtpm -y
+
+
+usermod -a -G libvirt ppksgopin
+usermod -a -G kvm ppksgopin
+usermod -a -G input ppksgopin
+
+systemctl enable --now libvirtd && systemctl start libvirtd
+sudo virsh net-start default
+sudo virsh net-autostart default
+
+apt install tree p7zip-full -y
+echo "ppksgopin  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ppksgopin
+
+
+apt-get update -y && apt --fix-broken install -y && apt-get upgrade -y
+
