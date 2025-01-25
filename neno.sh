6031855c768601d9413e8870ec8ec3793f8e1558
蔡孟儒 <ppksgopin@gmail.com>
2025-01-25 08:23:47 +0800

    Update neno.sh

--- neno.sh
+++ neno.sh
@@ -7,12 +7,12 @@ apt-get install ibus-chewing -y
 
 apt purge kate -y && apt autoremove -y
 
-apt install qemu-system libguestfs-tools libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf swtpm -y
+apt install linux-headers-generic build-essential dkms git -y
 
+apt install qemu-system libguestfs-tools libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf swtpm -y
 usermod -a -G libvirt ppksgopin
 usermod -a -G kvm ppksgopin
 usermod -a -G input ppksgopin
-
 systemctl enable --now libvirtd && systemctl start libvirtd
 sudo virsh net-start default
 sudo virsh net-autostart default
@@ -22,6 +22,7 @@ echo "ppksgopin  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ppksgopin
 
 apt install ssh firewalld -y
 systemctl start ssh && systemctl enable --now ssh
+systemctl start firewalld && systemctl enable --now firewalld
 
 apt-get update -y && apt --fix-broken install -y && apt-get upgrade -y
 
