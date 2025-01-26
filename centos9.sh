b2ef039ed1b500f6209b8c08e70c43790aa35166
蔡孟儒 <ppksgopin@gmail.com>
2025-01-26 16:16:23 +0800

    Create centos9.sh

---
+++ centos9.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+sudo dnf groupinstall "Virtualization Host" -y
+sudo systemctl enable --now libvirtd && sudo systemctl start libvirtd
+sudo dnf -y install virt-top libguestfs-tools
+lsmod | grep kvm
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker && sudo systemctl enable --now docker
