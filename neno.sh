58dda349442178f83f2fe4deeadd44dc732d09fd
蔡孟儒 <ppksgopin@gmail.com>
2025-02-01 15:15:48 +0800

    Update neno.sh

--- neno.sh
+++ neno.sh
@@ -5,26 +5,18 @@ mkdir /etc/apt/apt.conf.d/off && mv /etc/apt/apt.conf.d/20apt-esm-hook.conf /etc
 apt-get update -y && -y && apt-get upgrade -y
 
 apt purge kate -y && apt autoremove -y
-apt install linux-headers-generic build-essential dkms -y
 
-apt install qemu-system libguestfs-tools libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf swtpm -y
+apt install qemu-system libguestfs-tools libvirt-clients libvirt-daemon-system bridge-utils virt-manager -y
 usermod -a -G libvirt ppksgopin
 usermod -a -G kvm ppksgopin
-usermod -a -G input ppksgopin
 systemctl enable --now libvirtd && systemctl start libvirtd
-sudo virsh net-start default
-sudo virsh net-autostart default
 
-apt install tree p7zip-full -y
-echo "ppksgopin  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ppksgopin
-
-apt install ssh firewalld -y
+apt install ssh firewalld tree p7zip-full -y
 systemctl start ssh && systemctl enable --now ssh
 systemctl start firewalld && systemctl enable --now firewalld
 
-apt-get update -y && apt --fix-broken install -y && apt-get upgrade -y
+echo "ppksgopin  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ppksgopin
 
-sudo apt-get update -y && sudo apt upgrade -y
 sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -34,7 +26,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update -y && sudo apt upgrade -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-reboot
+apt-get update -y && -y && apt-get upgrade -y
